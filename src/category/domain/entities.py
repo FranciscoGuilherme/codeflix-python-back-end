970a729e84af65007f9d9ef4fb1fe85b22e25f16
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 09:17:13 -0300

    feat(domain): :construction: definicao de mais atributos na entidade categoria.

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
 class Category:
-    def __init__(self, name) -> None:
-        self.name = name
+    def __init__(self, name: str, description: str, is_active: bool, created_at: datetime) -> None:
+        self.name: str = name
+        self.description: str = description
+        self.is_active: bool = is_active
+        self.created_at: datetime = created_at
