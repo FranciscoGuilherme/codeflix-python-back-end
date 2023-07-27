67bd559e5f0cea2cd568cae722f451ecd6312147
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 20:52:05 -0300

    test(domain): :white_check_mark: adiciona mais verificacoes no teste do construtor.

--- src/category/tests/domain/test_entities.py
+++ src/category/tests/domain/test_entities.py
@@ -5,4 +5,7 @@ from category.domain.entities import Category
 class TestCategory(unittest.TestCase):
     def test_constructor(self):
         category = Category("Movie", "some description", True, datetime.now())
-        self.assertEqual(category.name, "Movie1")
+        self.assertEqual(category.name, "Movie")
+        self.assertEqual(category.description, "some description")
+        self.assertTrue(category.is_active)
+        self.assertIsInstance(category.created_at, datetime)
