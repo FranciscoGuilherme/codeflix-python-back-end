b0e93c086b55cf19c830939b87d06e272c69797c
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 21:28:46 -0300

    test(domain): :white_check_mark: valida se a classe implementa o dataclass.

--- src/category/tests/domain/test_entities.py
+++ src/category/tests/domain/test_entities.py
@@ -1,8 +1,13 @@
 import unittest
 from datetime import datetime
+from dataclasses import is_dataclass
 from category.domain.entities import Category
 
 class TestCategory(unittest.TestCase):
+
+    def test_if_is_a_dataclass(self):
+        self.assertTrue(is_dataclass(Category))
+
     def test_constructor(self):
         category = Category(
             name="Movie",
