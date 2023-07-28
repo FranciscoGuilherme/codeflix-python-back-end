94265d8dc46127b797d62b6a79fda1bf234d7791
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 21:19:17 -0300

    test(domain): :white_check_mark: trabalha a semantica, o arrange e o act.

--- src/category/tests/domain/test_entities.py
+++ src/category/tests/domain/test_entities.py
@@ -4,7 +4,12 @@ from category.domain.entities import Category
 
 class TestCategory(unittest.TestCase):
     def test_constructor(self):
-        category = Category("Movie", "some description", True, datetime.now())
+        category = Category(
+            name="Movie",
+            description="some description",
+            is_active=True,
+            created_at=datetime.now()
+        )
         self.assertEqual(category.name, "Movie")
         self.assertEqual(category.description, "some description")
         self.assertTrue(category.is_active)
