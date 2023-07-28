99bb26410ae8201b6e469eae4f1d7115918b1c81
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 21:14:48 -0300

    refactor(domain): :recycle: corrige identacao.

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -8,5 +8,7 @@ class Category:
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
-    created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now()
+    )
 
