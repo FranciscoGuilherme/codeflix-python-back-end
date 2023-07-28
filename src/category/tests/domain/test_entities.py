d4452622b4543cb7dddd7b8d8dab5db7b82b3846
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-27 21:41:28 -0300

    test(domain): :white_check_mark: implementa novos testes de unidade para validar os atributos apos construtor.

--- src/category/tests/domain/test_entities.py
+++ src/category/tests/domain/test_entities.py
@@ -8,14 +8,29 @@ class TestCategory(unittest.TestCase):
     def test_if_is_a_dataclass(self):
         self.assertTrue(is_dataclass(Category))
 
-    def test_constructor(self):
+    def test_constructor(self) -> None:
         category = Category(
             name="Movie",
             description="some description",
-            is_active=True,
+            is_active=False,
             created_at=datetime.now()
         )
         self.assertEqual(category.name, "Movie")
         self.assertEqual(category.description, "some description")
+        self.assertFalse(category.is_active)
+        self.assertIsInstance(category.created_at, datetime)
+
+    def test_constructor_default_data(self) -> None:
+        category = Category(name="Movie")
+        self.assertEqual(category.name, "Movie")
+        self.assertEqual(category.description, None)
         self.assertTrue(category.is_active)
         self.assertIsInstance(category.created_at, datetime)
+
+    def test_if_created_at_is_generated_in_constructor(self) -> None:
+        category1 = Category(name="Movie 1")
+        category2 = Category(name="Movie 2")
+        self.assertNotEqual(
+            category1.created_at.timestamp(),
+            category2.created_at.timestamp()
+        )
