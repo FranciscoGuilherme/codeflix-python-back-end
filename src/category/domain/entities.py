22b305122feb3140e2f8883627689151ceca9c23
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-29 18:37:27 -0300

    feat(domain): :sparkles: implementa a estrategia do 'kw_only' para a definicao do ID.

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,14 +1,17 @@
+import uuid
 from typing import Optional
 from datetime import datetime
 from dataclasses import dataclass, field
 
-@dataclass()
+@dataclass(kw_only=True)
 class Category:
 
+    id: uuid.UUID = field(
+        default_factory=lambda: uuid.uuid4()
+    )
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(
         default_factory=lambda: datetime.now()
     )
-
