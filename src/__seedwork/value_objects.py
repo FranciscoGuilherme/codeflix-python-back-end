ba90166f32fcb53f5ce8535c90101f36a9eea1ec
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-30 12:50:20 -0300

    feat(value_objects): :sparkles: implementa o objeto de valor para representar o ID.

---
+++ src/__seedwork/value_objects.py
@@ -0,0 +1,20 @@
+import uuid
+from dataclasses import dataclass, field
+from __seedwork.exceptions import InvalidUuidException
+
+@dataclass
+class UniqueEntityId:
+
+    id: str = field(
+        default_factory=lambda: str(uuid.uuid4())
+    )
+
+    def __post_init__(self) -> None:
+        self.id = str(self.id) if isinstance(self.id, uuid.UUID) else self.id
+        self.__validate()
+
+    def __validate(self) -> None:
+        try:
+            uuid.UUID(self.id)
+        except ValueError as ex:
+            raise InvalidUuidException from ex
