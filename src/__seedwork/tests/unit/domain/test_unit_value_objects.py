6f38d232d134e5363db9c9f913588830127aa764
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-30 17:25:49 -0300

    test(value_objects): :white_check_mark: implementa o teste de unidade referente ao metodo de conversao do objeto para string.

--- src/__seedwork/tests/unit/domain/test_unit_value_objects.py
+++ src/__seedwork/tests/unit/domain/test_unit_value_objects.py
@@ -53,3 +53,7 @@ class TestUniqueEntityIdUnit(unittest.TestCase):
         with self.assertRaises(FrozenInstanceError):
             value_object = UniqueEntityId()
             value_object.id = "fake id"
+
+    def test_convert_to_str(self) -> None:
+        value_object = UniqueEntityId()
+        self.assertEqual(value_object.id, str(value_object))
