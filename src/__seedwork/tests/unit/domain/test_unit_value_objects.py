b0b8223910ebb8db8e3b7ba63947b3ef4b4f766d
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-30 18:17:41 -0300

    test(value_objects): :white_check_mark: implementa os testes de unidade referentes a abstracao.

--- src/__seedwork/tests/unit/domain/test_unit_value_objects.py
+++ src/__seedwork/tests/unit/domain/test_unit_value_objects.py
@@ -1,12 +1,59 @@
 import uuid
 import unittest
+from abc import ABC
 from unittest.mock import patch
-from dataclasses import is_dataclass, FrozenInstanceError
+from dataclasses import dataclass, is_dataclass, FrozenInstanceError
 from __seedwork.domain.exceptions import InvalidUuidException
-from __seedwork.domain.value_objects import UniqueEntityId
+from __seedwork.domain.value_objects import ValueObject, UniqueEntityId
 
+
+@dataclass(frozen=True)
+class StubOneProp(ValueObject):
+    prop: str
+
+
+@dataclass(frozen=True)
+class StubTwoProps(ValueObject):
+    prop1: str
+    prop2: str
+
+
+class TestValueObjectUnit(unittest.TestCase):
+
+    def test_if_is_a_dataclass(self) -> None:
+        self.assertTrue(is_dataclass(ValueObject))
+
+    def test_if_is_instance_of_abstract_class(self) -> None:
+        self.assertIsInstance(ValueObject(), ABC)
+
+    def test_init_prop(self) -> None:
+        value_object_one_prop = StubOneProp(prop="message")
+        self.assertEqual(value_object_one_prop.prop, "message")
+
+        value_object_two_props = StubTwoProps(
+            prop1="custom value 1",
+            prop2="custom value 2"
+        )
+        self.assertEqual(value_object_two_props.prop1, "custom value 1")
+        self.assertEqual(value_object_two_props.prop2, "custom value 2")
+
+    def test_convert_to_str(self) -> None:
+        value_object_one_prop = StubOneProp(prop="custom value")
+        self.assertEqual(value_object_one_prop.prop, str(value_object_one_prop))
+
+        value_object_two_props = StubTwoProps(
+            prop1="message 1",
+            prop2="message 2"
+        )
+        self.assertEqual('{"prop1": "message 1", "prop2": "message 2"}', str(value_object_two_props))
+
+    def test_is_immutable(self) -> None:
+        with self.assertRaises(FrozenInstanceError):
+            value_object = StubOneProp(prop="custom value")
+            value_object.prop = "custom modified"
 
 class TestUniqueEntityIdUnit(unittest.TestCase):
+
     def test_if_is_a_dataclass(self) -> None:
         self.assertTrue(is_dataclass(UniqueEntityId))
 
@@ -53,7 +100,3 @@ class TestUniqueEntityIdUnit(unittest.TestCase):
         with self.assertRaises(FrozenInstanceError):
             value_object = UniqueEntityId()
             value_object.id = "fake id"
-
-    def test_convert_to_str(self) -> None:
-        value_object = UniqueEntityId()
-        self.assertEqual(value_object.id, str(value_object))
