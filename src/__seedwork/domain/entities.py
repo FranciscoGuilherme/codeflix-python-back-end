74999ce78c941f04d024f2953f1c3e322b5eeb37
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-06 15:48:42 -0300

    feat(seedwork): :sparkles: implementa a abstracao de entidade.

---
+++ src/__seedwork/domain/entities.py
@@ -0,0 +1,21 @@
+from abc import ABC
+from dataclasses import dataclass, field, asdict
+from __seedwork.domain.value_objects import UniqueEntityId
+
+
+@dataclass(frozen=True)
+class Entity(ABC):
+
+    unique_entity_id: UniqueEntityId = field(
+        default_factory=lambda: UniqueEntityId()
+    )
+
+    @property
+    def id(self) -> str:
+        return str(self.unique_entity_id)
+
+    def to_dict(self) -> dict:
+        entity_dict = asdict(self)
+        entity_dict.pop("unique_entity_id")
+        entity_dict["id"] = self.id
+        return entity_dict
