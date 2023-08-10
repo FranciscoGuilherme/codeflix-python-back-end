2d2a087f18b18698e91bf227674eb34d2c50999e
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-10 10:59:10 -0300

    feat(seedwork): :construction: implementacao inicial da exception customizada.

--- src/__seedwork/domain/exceptions.py
+++ src/__seedwork/domain/exceptions.py
@@ -1,3 +1,7 @@
 class InvalidUuidException(Exception):
     def __init__(self, error = "ID must be a valid UUID") -> None:
         super().__init__(error)
+
+
+class ValidationException(Exception):
+    pass
