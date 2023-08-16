e8ed032b1cfb10fafda61ae1e2bcc92c8442605d
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-16 11:08:23 -0300

    feat(entities): :construction: implementacao parcial do validator na entidade Categoria.

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -2,7 +2,7 @@ from typing import Optional
 from datetime import datetime
 from dataclasses import dataclass, field
 from __seedwork.domain.entities import Entity
-from __seedwork.domain.validators import ValidatorRules
+from category.domain.validators import CategoryValidatorFactory
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -19,9 +19,14 @@ class Category(Entity):
         cls.validate(
             name=kwargs.get("name"),
             description=kwargs.get("description"),
-            is_active=kwargs.get("is_active")
+            is_active=kwargs.get("is_active"),
+            created_at=kwargs.get("created_at")
         )
         return super(Category, cls).__new__(cls)
+
+    def __post_init__(self):
+        if not self.created_at:
+            object.__setattr__(self, "created_at", datetime.now())
 
     def update(self, name: str, description: Optional[str] = None) -> None:
         self.validate(name, description)
@@ -38,7 +43,11 @@ class Category(Entity):
         self._set("is_active", False)
 
     @classmethod
-    def validate(cls, name: str, description: str = None, is_active: bool = None) -> None:
-        ValidatorRules.values(name, "name").required().string().max_length(255)
-        ValidatorRules.values(description, "description").string()
-        ValidatorRules.values(is_active, "is_active").boolean()
+    def validate(cls, name: str, description: str = None, is_active: bool = None, created_at: datetime = None) -> None:
+        validator = CategoryValidatorFactory.create()
+        is_valid = validator.validate({
+            "name": name,
+            "description": description,
+            "is_active": is_active,
+            "created_at": created_at
+        })
