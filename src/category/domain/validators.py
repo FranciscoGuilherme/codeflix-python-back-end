1a9e64304351e079c03ad82d5b9d581d3f047756
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-16 11:24:20 -0300

    feat(validators): :sparkles: aplica as novas validacoes sobrescritas para a entidade Categoria.

--- src/category/domain/validators.py
+++ src/category/domain/validators.py
@@ -1,12 +1,12 @@
 from typing import Any
 from rest_framework import serializers
-from __seedwork.domain.validators import DRFValidator
+from __seedwork.domain.validators import DRFValidator, StrictCharField, StrictBoolField
 
 
 class CategoryRules(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField(required=False, allow_null=True)
-    is_active = serializers.BooleanField(required=False)
+    name = serializers.StrictCharField(max_length=255)
+    description = serializers.StrictCharField(required=False, allow_null=True)
+    is_active = serializers.StrictBoolField(required=False)
     created_at = serializers.DateTimeField(required=False)
 
 
