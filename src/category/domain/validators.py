a7414159486907340af1201656b147d72aad6954
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-08-25 14:07:24 -0300

    fix(validators): :bug: corrige a definicao das regras para os atributos da categoria.

--- src/category/domain/validators.py
+++ src/category/domain/validators.py
@@ -1,19 +1,19 @@
-from typing import Any
+from typing import Dict
 from rest_framework import serializers
 from __seedwork.domain.validators import DRFValidator, StrictCharField, StrictBoolField
 
 
 class CategoryRules(serializers.Serializer):
-    name = serializers.StrictCharField(max_length=255)
-    description = serializers.StrictCharField(required=False, allow_null=True)
-    is_active = serializers.StrictBoolField(required=False)
+    name = StrictCharField(max_length=255)
+    description = StrictCharField(required=False, allow_null=True)
+    is_active = StrictBoolField(required=False)
     created_at = serializers.DateTimeField(required=False)
 
 
 class CategoryValidator(DRFValidator):
 
-    def validate(self, data: Any):
-        rules = CategoryRules(data=data)
+    def validate(self, data: Dict):
+        rules = CategoryRules(data=data if data is not None else {})
         return super().validate(rules)
 
 
